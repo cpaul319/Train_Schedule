d128a2867b4bdd8fd3520f811c62d8527c8e8d68
Chris Paul <cpaul319@users.noreply.github.com>
2019-03-12 17:15:39 -0400

    may be final update to homework

--- assets/javascript/moment.js
+++ assets/javascript/moment.js
@@ -1,25 +1,27 @@
 
-  // Initialize Firebase
+ 
+  var recordCount = 0;
   var config = {
     apiKey: "AIzaSyABTQoxl_fHMHFLQOusalMaPdeeyflTPc8",
     authDomain: "train-schedule-b67e0.firebaseapp.com",
     databaseURL: "https://train-schedule-b67e0.firebaseio.com",
-    projectId: "train-schedule-b67e0",
+ 
     storageBucket: "train-schedule-b67e0.appspot.com",
-    messagingSenderId: "848220802901"
+    
   };
   firebase.initializeApp(config);
 
   var database = firebase.database();
 
-  $("#submit-train-btn").on("click", function (event) {
+  $("#add_train").on("click", function (event) {
     event.preventDefault();
 
-    var trainName = $("#train-name").val().trim();
-    var destination = $("#train-destination").val().trim();
-    var firstTrain = $("#train-time").val().trim();
+    var trainName = $("#train_name").val().trim();
+    var destination = $("#train_destination").val().trim();
+    var firstTrain = $("#train_time").val().trim();
     var frequency = $("#train_frequency").val().trim();
     recordCount = 0;
+    console.log(recordCount);
 
     database.ref().push({
         name: trainName,
@@ -31,16 +33,53 @@
   
     alert("Train Added");
 
-    // Clears all of the text-boxes
-    $("#train-name").val("");
-    $("#train-destination").val("");
-    $("#train-time").val("");
+    // resets the form
+    $("#train_name").val("");
+    $("#train_destination").val("");
+    $("#train_time").val("");
     $("#train_frequency").val("");
+     
 });
-// var trainTime;   //  This variable holds first train arrival time
-// var currentTime;       //  This variable holds current time
-// var nextArrival;       //  This variable holds next arrival time
-// var minutesAway;       //  This variable holds how many minutes away is the next train
+ 
+
+database.ref().on("child_added", function (document) {
+    recordCount += 1;
+
+    console.log(document.key);
+    console.log(document.val());
+
+    var name = document.val().name;
+    var destination = document.val().destination;
+    var frequency = document.val().frequency;
+    var firstTrain = document.val().firstTrain;
+    var arrivalMinutes;
+    var arrivalTime;
+
+    
+    var trainTime = moment(firstTrain, "hh:mm").subtract(1, "years");
+
+    //number of minutes between first train and now
+    var minuteDifference = moment().diff(moment(trainTime), "minutes");
+    var remainder = minuteDifference % frequency;
+    arrivalMinutes = frequency - remainder;
+
+    var nextTrain = moment().add(arrivalMinutes, "minutes");
+    arrivalTime = moment(nextTrain).format("hh:mm");
+
+
+    $("#train-table > tbody").append(
+        $("<tr>").append(
+          
+            $("<td>").text(name),
+            $("<td>").text(destination),
+            $("<td>").text(frequency),
+            $("<td>").text(arrivalTime),
+            $("<td>").text(arrivalMinutes)
+        )
+    );
+
+    console.log("Record:" + recordCount);
+});
  
 
 
