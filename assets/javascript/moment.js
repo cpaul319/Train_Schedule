1ad0bac6e0383c7109842a33b6f995fbb3457962
Chris Paul <cpaul319@users.noreply.github.com>
2019-03-12 13:37:49 -0400

    update

--- assets/javascript/moment.js
+++ assets/javascript/moment.js
@@ -1,20 +1,46 @@
 
   // Initialize Firebase
   var config = {
-    apiKey: "AIzaSyDU8yfZlU6JfVOdMplTGWMYvCftrp1iSrY",
-    authDomain: "timesheet-d1706.firebaseapp.com",
-    databaseURL: "https://timesheet-d1706.firebaseio.com",
-    projectId: "timesheet-d1706",
-    storageBucket: "timesheet-d1706.appspot.com",
-    messagingSenderId: "1059669218531"
+    apiKey: "AIzaSyABTQoxl_fHMHFLQOusalMaPdeeyflTPc8",
+    authDomain: "train-schedule-b67e0.firebaseapp.com",
+    databaseURL: "https://train-schedule-b67e0.firebaseio.com",
+    projectId: "train-schedule-b67e0",
+    storageBucket: "train-schedule-b67e0.appspot.com",
+    messagingSenderId: "848220802901"
   };
   firebase.initializeApp(config);
 
   var database = firebase.database();
-  var trainName;         //  This variable holds the name of the train
-var destination;        //  This variable holds the destination
-var trainTime;   //  This variable holds first train arrival time
-var currentTime;       //  This variable holds current time
-var nextArrival;       //  This variable holds next arrival time
-var minutesAway;       //  This variable holds how many minutes away is the next train
-var frequency;  
+
+  $("#submit-train-btn").on("click", function (event) {
+    event.preventDefault();
+
+    var trainName = $("#train-name").val().trim();
+    var destination = $("#train-destination").val().trim();
+    var firstTrain = $("#train-time").val().trim();
+    var frequency = $("#train_frequency").val().trim();
+    recordCount = 0;
+
+    database.ref().push({
+        name: trainName,
+        destination: destination,
+        firstTrain: firstTrain,
+        frequency: frequency
+    });
+
+  
+    alert("Train Added");
+
+    // Clears all of the text-boxes
+    $("#train-name").val("");
+    $("#train-destination").val("");
+    $("#train-time").val("");
+    $("#train_frequency").val("");
+});
+// var trainTime;   //  This variable holds first train arrival time
+// var currentTime;       //  This variable holds current time
+// var nextArrival;       //  This variable holds next arrival time
+// var minutesAway;       //  This variable holds how many minutes away is the next train
+ 
+
+
